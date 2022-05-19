678afa750d16154eebf0a82334b0686aa1796bbf
이상진 <idealjin316@gmail.com>
2022-05-19 17:01:47 +0900

    update: ~188p

---
+++ src/main/java/com/sangjin/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,20 @@
+package com.sangjin.book.springboot.config.auth.dto;
+
+import com.sangjin.book.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+
+}

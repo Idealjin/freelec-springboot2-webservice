2ada25187c2796bc195f58fda6ae014ff2370724
이상진 <idealjin316@gmail.com>
2022-05-24 17:44:04 +0900

    update: ~200p

--- src/main/java/com/sangjin/book/springboot/web/IndexController.java
+++ src/main/java/com/sangjin/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.sangjin.book.springboot.web;
 
+import com.sangjin.book.springboot.config.auth.LoginUser;
 import com.sangjin.book.springboot.config.auth.dto.SessionUser;
 import com.sangjin.book.springboot.service.posts.PostsService;
 import com.sangjin.book.springboot.web.dto.PostsResponseDto;
@@ -9,19 +10,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public  String index(Model model){
+    public  String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
